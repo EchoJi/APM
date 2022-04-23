b4a991be1f6c234b62ba7520ba6e9ee5b1338927
MAN\EJI <echo.ji@coxautoinc.com>
2022-04-23 10:34:00 +1000

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { ProductListComponent } from './products/product-list.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent, ProductListComponent],
+  imports: [BrowserModule, FormsModule],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
